671a0cdd984e18f072e3d44f28b7a0634f82534e
Jenny <hzn2017@outlook.com>
2019-03-29 16:19:55 +0800

    Delete demo.js

--- demo.js
+++
@@ -1,6 +0,0 @@
-// put this file in your folder full of csv files
-// node demo.js
-// then your csv files are merged
-const mergecsv = require('mergecsv');
-
-mergecsv(__dirname, "new_file_name.csv");
